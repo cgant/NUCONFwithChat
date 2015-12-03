ee358af40316b6aa07d7cc7a8efa869844666dd9
cgant <candice.m.gant@gmail.com>
2015-12-02 18:02:54 -0600

    changes to server.js

--- server.js
+++ server.js
@@ -116,6 +116,7 @@ app.post('/api/register', function (req, res, next) {
         res.send(200);
     });
 });
+
 app.get('/api/logout', function (req, res, next) {
     req.logout();
     res.send(200);
@@ -134,3 +135,51 @@ app.get('/api/events', function (req, res, next) {
         res.send(shows);
     });
 });
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(
+    200,
+    {"content-type": mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+  if (cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function(exists) {
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+  if (request.url == '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + request.url;
+  }
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
+});
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
